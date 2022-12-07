293ec21815b2277a2f6b1a9675f2141e421ff12b
Juvencio Linhares <juvlinhares@gmail.com>
2022-12-07 00:11:17 -0300

    delecao de usuario com DELETE

--- src/main/java/com/juvlinhares/workshopmongo/services/UserService.java
+++ src/main/java/com/juvlinhares/workshopmongo/services/UserService.java
@@ -30,7 +30,14 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+
+	public void delete(String id) {
+		findById(id);//tratamento de exceção, caso o id não exista
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	
 }
