bb26bdec4c8afc964cf9ed32f6715dc99f50c689
Juvencio Linhares <juvlinhares@gmail.com>
2022-12-11 19:33:34 -0300

    projecao dos dados do autor com DTO

--- src/main/java/com/juvlinhares/workshopmongo/config/Instantiation.java
+++ src/main/java/com/juvlinhares/workshopmongo/config/Instantiation.java
@@ -10,42 +10,42 @@ import org.springframework.context.annotation.Configuration;
 
 import com.juvlinhares.workshopmongo.domain.Post;
 import com.juvlinhares.workshopmongo.domain.User;
+import com.juvlinhares.workshopmongo.dto.AuthorDTO;
 import com.juvlinhares.workshopmongo.repository.PostRepository;
 import com.juvlinhares.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-		//injetar o UserRepository p fazer a operação com o bd: 
-	
+	// injetar o UserRepository p fazer a operação com o bd:
+
 	@Autowired
-	private UserRepository userRepository;//injeção do user repository
-	
+	private UserRepository userRepository;// injeção do user repository
+
 	@Autowired
-	private PostRepository postRepository;//injeção do user repository
-	
+	private PostRepository postRepository;// injeção do user repository
+
 	@Override
 	public void run(String... args) throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		//limpar o bd:
+		// limpar o bd:
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
-		//instanciar o obj e salvar no bd:
+
+		// instanciar o obj e salvar no bd:
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		//salva novos Users no bd
+
+		// salvar novos Users no bd
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		//salvar os posts no bd
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		// salvar os posts no bd
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-		
+
 	}
 
 }
---
+++ src/main/java/com/juvlinhares/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,43 @@
+package com.juvlinhares.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.juvlinhares.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name; 
+	
+	public AuthorDTO() {
+		
+	}
+
+	public AuthorDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
